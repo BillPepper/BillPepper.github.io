4c173c583355aca7396e87b9b46104f748e8a6df
Mattes Wenzel <mattes@artfacts.net>
2019-04-09 19:08:10 +0200

    use old page

---
+++ precache-manifest.28c7da3a8b0592dcedd214e5f47e79eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc468264c8d9bf2dcc60",
+    "url": "/static/css/main.1057d337.chunk.css"
+  },
+  {
+    "revision": "bc468264c8d9bf2dcc60",
+    "url": "/static/js/main.bc468264.chunk.js"
+  },
+  {
+    "revision": "952cce579303eecc2615",
+    "url": "/static/js/1.952cce57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b570cf4b53208a03c5b4421045726f73",
+    "url": "/static/media/twitter-logo.b570cf4b.svg"
+  },
+  {
+    "revision": "2114e31e4c84f954ebe7fe55e36ac2f6",
+    "url": "/static/media/facebook-logo.2114e31e.svg"
+  },
+  {
+    "revision": "bb14284aac969782f60f82a93952fd1d",
+    "url": "/static/media/linkedin-logo.bb14284a.svg"
+  },
+  {
+    "revision": "8bff3dd949b8bb55c3d4a12b517b2d93",
+    "url": "/static/media/soundcloud-logo.8bff3dd9.svg"
+  },
+  {
+    "revision": "b015a127987068a25358595aa148297e",
+    "url": "/static/media/pp.b015a127.jpg"
+  },
+  {
+    "revision": "436440a9d3a04672b7b611bd2b5b8df4",
+    "url": "/static/media/bg.436440a9.jpg"
+  },
+  {
+    "revision": "555344ce40bf0e161213e832b84acfbe",
+    "url": "/index.html"
+  }
+];
